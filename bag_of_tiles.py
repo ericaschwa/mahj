ab0bc4094f36152e727195c973f2e3d9930cf5e8
Erica Schwartz <erica.schwartz.4@gmail.com>
2023-08-05 12:02:54 -0400

    added the ability to deal tiles

--- bag_of_tiles.py
+++ bag_of_tiles.py
@@ -19,32 +19,33 @@ class BagOfTiles:
 			Tile("DOT_9", 4,  8),
 			Tile("BAM_1", 4,  9),
 			Tile("BAM_2", 4, 10),
-			Tile("BAM_3", 4, 12),
-			Tile("BAM_4", 4, 13),
-			Tile("BAM_5", 4, 14),
-			Tile("BAM_6", 4, 15),
-			Tile("BAM_7", 4, 16),
-			Tile("BAM_8", 4, 17),
-			Tile("BAM_9", 4, 18),
-			Tile("CRA_1", 4, 19),
-			Tile("CRA_2", 4, 20),
-			Tile("CRA_3", 4, 21),
-			Tile("CRA_4", 4, 22),
-			Tile("CRA_5", 4, 23),
-			Tile("CRA_6", 4, 24),
-			Tile("CRA_7", 4, 25),
-			Tile("CRA_8", 4, 26),
-			Tile("CRA_9", 4, 27),
-			Tile("NORTH", 4, 28),
-			Tile("EAST_", 4, 29),
-			Tile("WEST_", 4, 30),
-			Tile("SOUTH", 4, 31),
-			Tile("SOAP_", 4, 32),
-			Tile("GREEN", 4, 33),
-			Tile("RED__", 4, 34),
-			Tile("FLOWR", 8, 35),
-			Tile("JOKER", 8, 36),
+			Tile("BAM_3", 4, 11),
+			Tile("BAM_4", 4, 12),
+			Tile("BAM_5", 4, 13),
+			Tile("BAM_6", 4, 14),
+			Tile("BAM_7", 4, 15),
+			Tile("BAM_8", 4, 16),
+			Tile("BAM_9", 4, 17),
+			Tile("CRA_1", 4, 18),
+			Tile("CRA_2", 4, 19),
+			Tile("CRA_3", 4, 20),
+			Tile("CRA_4", 4, 21),
+			Tile("CRA_5", 4, 22),
+			Tile("CRA_6", 4, 23),
+			Tile("CRA_7", 4, 24),
+			Tile("CRA_8", 4, 25),
+			Tile("CRA_9", 4, 26),
+			Tile("NORTH", 4, 27),
+			Tile("EAST_", 4, 28),
+			Tile("WEST_", 4, 29),
+			Tile("SOUTH", 4, 30),
+			Tile("SOAP_", 4, 31),
+			Tile("GREEN", 4, 32),
+			Tile("RED__", 4, 33),
+			Tile("FLOWR", 8, 34),
+			Tile("JOKER", 8, 35),
 		]
+		self.update_ungrouped_bag()
 
 	def __str__(self):
 		return '\n'.join([str(t) for t in self.bag])
@@ -52,14 +53,22 @@ class BagOfTiles:
 	def zero_out(self):
 		for t in self.bag:
 			t.zero_out()
+		self.update_ungrouped_bag()
 
 	def increment(self,  tile_id):
 		self.bag[tile_id].increment()
+		self.update_ungrouped_bag()
 
 	def decrement (self, tile_id):
 		self.bag[tile_id].decrement()
+		self.update_ungrouped_bag()
 
 	def simplified_str(self):
 		ret = '\n'.join([t.simplified_str() for t in self.bag if t.count > 0])
 		if len(ret) == 0: return "Empty"
 		return ret
+
+	def update_ungrouped_bag(self):
+		self.ungrouped_bag = []
+		for tile in self.bag:
+			self.ungrouped_bag += [tile.tile_id] * tile.count
--- environment.py
+++ environment.py
@@ -4,7 +4,7 @@ from bag_of_tiles import BagOfTiles
 import random
 
 # Note that East has one more, and one more is needed for the final hands
-TILES_PER_PLAYERS = 13
+TILES_PER_PLAYER = 13
 NUM_PLAYERS = 4
 
 class Environment:
@@ -16,16 +16,35 @@ class Environment:
 			self.east_player = east_player
 		else:
 			self.east_player = random.choice(range(NUM_PLAYERS))
-		self.hands = [BagOfTiles()] * NUM_PLAYERS
-		# Currently all tiles are hidden : zero out the visible tiles and the hands
+		self.hands = []
+		for i in range(NUM_PLAYERS):
+			self.hands.append(BagOfTiles())
+		# First, all tiles are hidden : zero out the visible tiles and the hands
 		self.visible_tiles.zero_out()
 		for hand in self.hands:
 			hand.zero_out()
+		# Then, deal.
+		self.deal()
 
 	def __str__(self):
 		ret = f"East: {self.east_player}\n"
-		ret += f"Visible Tiles:\n{self.visible_tiles.simplified_str()}\n"
-		ret += f"Hiddden Tiles:\n{self.hidden_tiles.simplified_str()}\n"
+		ret += f"Visible Tiles:\n{self.visible_tiles.simplified_str()}\n\n"
+		ret += f"Hiddden Tiles:\n{self.hidden_tiles.simplified_str()}\n\n"
 		for player in range(NUM_PLAYERS):
-			ret += f"Player {player}: {self.hands[player].simplified_str()}\n"
+			ret += f"Player {player}:\n{self.hands[player].simplified_str()}\n\n"
 		return ret
+
+	def deal(self):
+		for player in range(NUM_PLAYERS):
+			for i in range(TILES_PER_PLAYER):
+				chosen_tile_id = random.choice(self.hidden_tiles.ungrouped_bag)
+				self.hands[player].increment(chosen_tile_id)
+				self.hidden_tiles.decrement(chosen_tile_id)
+
+		# Give one extra to East.
+		chosen_tile_id = random.choice(self.hidden_tiles.ungrouped_bag)
+		self.hands[self.east_player].increment(chosen_tile_id)
+		self.hidden_tiles.decrement(chosen_tile_id)
+
+e = Environment()
+print(e)
